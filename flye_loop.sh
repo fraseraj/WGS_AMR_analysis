970340d5afb9d80e7f59f7f87d60b1c83f3e4995
fraseraj <94050739+fraseraj@users.noreply.github.com>
2024-01-24 15:53:35 +0000

    Create flye_loop.sh
    Loop using Flye to create draft assemblies using ONT reads only.

---
+++ flye_loop.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Create assembly using flye and ONT read. Assemblies will later be polished using Illumina short reads. 
+# --nano-hq flag used as fast5 data was basecalled super accurately using Guppy5
+# code relies on each fastq file being located within a directory with the same ID number with the prefix 'assem_'
+# script must be run in the parent directory which contains all the individual directories which contain the ONT fatq reads
+
+
+assem_prefix="assem"
+
+for assem_dir in ${assem_prefix}*; do
+
+ #Extract the sample ID from the "assem" directory name
+  sample_id=${assem_dir#"$assem_prefix"}
+
+  echo "Generating Flye draft assembly for sample ${sample_id}"
+
+  #run flye
+  flye --nano-raw "${assem_dir}/QC${sample_id}.fastq.gz" --threads 16 --out-dir "${assem_dir}/flyedraftassem${sample_id}"
+
+  #if/else statement to check if flye has completed succesfull - based on checking the assembly.fasta file has been produced 
+  if [ -e "${assem_dir}/flyedraftassem${sample_id}/assembly.fasta" ]; then # -e is the test condition - checking if the assembly.fasta exists.
+
+    echo "Flye completed successfully for sample ${sample_id}"
+
+  else
+
+    echo "Error: Flye failed for sample ${sample_id}"
+
+    exit 1 #if the assembly.fasta file is not found the loop with terminate 
+
+  fi
+
+  #rename the assembly.fasta file based on the sample id
+  mv "${assem_dir}/flyedraftassem${sample_id}/assembly.fasta" "${assem_dir}/flyedraftassem${sample_id}/${sample_id}_flyedraftassem.fasta"
+
+
+done
